97e667ed792772b9455367be5a37ee8873a154f6
enricorobazza <enricorobazzi@gmail.com>
2019-10-01 00:10:25 -0300

    Fixed config.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
-const config = require('../../config')
+// const config = require('../../config')
 
 // const db = mysql.createConnection({
 //     host: 'localhost',
@@ -9,11 +9,16 @@ const config = require('../../config')
 //     database: 'basicsite'
 // });
 
-const {host, user, password, database} = config;
-host = process.env.HOST || host;
-user = process.env.USER || user;
-password = process.env.PASSWORD || password;
-database = process.env.DATABASE || database;
+// const {host, user, password, database} = config;
+// host = process.env.HOST || host;
+// user = process.env.USER || user;
+// password = process.env.PASSWORD || password;
+// database = process.env.DATABASE || database;
+
+host = process.env.HOST;
+user = process.env.USER;
+password = process.env.PASSWORD;
+database = process.env.DATABASE;
 
 const db = mysql.createConnection({ host, user, password, database });
 
